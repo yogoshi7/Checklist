cffb471347a640a4a7d24b6a3a3bc46b0bd039e1
yogoshi7 <banana.moonlight73@gmail.com>
2024-10-02 15:15:50 +0500

    Added List and Task classes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ListComponent} from "./list/list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css', '../../node_modules/bootstrap/dist/css/bootstrap.min.css'],
 })
 export class AppComponent {
   title = 'Checklist-App';
+
+
 }
