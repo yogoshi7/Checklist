4275177c6b2d8af268d7155628bfb7e15bbf3cbb
yogoshi7 <banana.moonlight73@gmail.com>
2024-10-03 15:51:52 +0500

    Final changes

--- src/app/task/component/task.component.ts
+++ src/app/task/component/task.component.ts
@@ -2,11 +2,14 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Task } from "../task";
 import { ListService } from "../../list/service/list.service";
 import { TaskService } from "../service/task.service";
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
   providers: [ListService, TaskService]
@@ -15,10 +18,8 @@ export class TaskComponent implements OnInit{
   @Input()
   //@ts-ignore
   public task: Task;
-  @Output()
-  public currValue: string = '';
+  public newValue: string = '';
   public prevValue: string = '';
-  @Input()
   public isEdit: boolean = false;
   @Output()
   public doDelete = new EventEmitter<void>();
@@ -27,24 +28,21 @@ export class TaskComponent implements OnInit{
   }
 
   public ngOnInit() {
-    
+
   }
 
   public editTask(): void {
     this.isEdit = true;
-    this.prevValue = this.currValue;
+    this.prevValue = this.newValue = this.task.body;
   }
 
   public deleteTask(): void {
     this.doDelete.emit();
   }
 
-  public saveTask(): void {
-
-  }
-
-  public cancelEdit() {
+  public cancelEdit(save: boolean = false) {
     this.isEdit = false;
+    this.task.body = save ? this.newValue : this.prevValue;
   }
 
   public completeTask() {
